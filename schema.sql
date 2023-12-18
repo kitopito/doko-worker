db1a9bdc3f487afab352899f3ab7b99a486a6546
kitopito <b19125@kagoshima.kosen-ac.jp>
2023-12-19 06:08:48 +0900

    first commit

---
+++ schema.sql
@@ -0,0 +1,57 @@
+-- Teacherテーブル
+CREATE TABLE Teacher (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    lastUpdate INTEGER,
+    status_id INTEGER,
+    FOREIGN KEY (status_id) REFERENCES Status(id)
+);
+
+-- Statusテーブル
+CREATE TABLE Status (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    status TEXT
+);
+
+-- Configテーブル
+CREATE TABLE Config (
+    teacher_id INTEGER,
+    status_id INTEGER,
+    sensor_id INTEGER,
+    config_value INTEGER,
+    PRIMARY KEY (teacher_id, status_id, sensor_id),
+    FOREIGN KEY (teacher_id) REFERENCES Teacher(id),
+    FOREIGN KEY (status_id) REFERENCES Status(id),
+    FOREIGN KEY (sensor_id) REFERENCES Sensor(id)
+);
+
+-- SensorValueテーブル
+CREATE TABLE Sensor (
+    id INTEGER PRIMARY KEY,
+    teacher_id INTEGER,
+    value INTEGER,
+    FOREIGN KEY (teacher_id) REFERENCES Teacher(id)
+);
+
+insert into Status (id, status) values 
+    (0, "在室"),
+    (1, "講義"),
+    (2, "出張"),
+    (3, "帰宅"),
+    (4, "会議"),
+    (5, "学内");
+
+insert into Teacher (id, name, status_id) values 
+    (0, "ほげ", 0),
+    (1, "谷口", 0),
+    (2, "瀬戸山", 1),
+    (3, "新田", 0),
+    (4, "小原", 3),
+    (5, "田中", 1),
+    (6, "山田", 2),
+    (7, "岡田", 0),
+    (8, "鈴木", 3),
+    (9, "高橋", 1),
+    (10, "佐藤", 2),
+    (11, "伊藤", 0),
+    (12, "渡辺", 3);
