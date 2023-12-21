45e8a17e47b95fce9ccc8cd476bea41a19adb1a8
kitopito <kitopitowada@gmail.com>
2023-12-21 16:04:11 +0900

    sensor post

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { Bindings } from 'hono/types';
 import { drizzle } from 'drizzle-orm/d1';
-import { status, teacher, users } from './schema';
+import { config, sensor, status, teacher, users } from './schema';
 import { eq } from 'drizzle-orm';
 
 /*
@@ -34,6 +34,7 @@ app.get('/teachers', async (c) => {
         id: teacher.id,
         name: teacher.name,
         status: status.status,
+        updatedAt: teacher.updatedAt,
     })
     .from(teacher)
     .leftJoin(status, eq(teacher.statusId, status.id));
@@ -43,7 +44,23 @@ app.get('/teachers', async (c) => {
 app.get('/status', async (c) => {
 });
 
-app.get('/sensors', async (c) => {
+app.post('/sensors', async (c) => {
+    const db = drizzle(c.env.DB);
+    const param = await c.req.json<{deviceAddress: Number, value: Array<number>}>();
+    console.log(param);
+//    const sensorValues = c.req.param('value');
+//    const teacherId = 2; // Mr.Taniguchi
+    const teacherId = 1; // hoge
+//    const configs = await db.select().from(config).where(eq(config.teacherId, teacherId));
+    const sensors = await db.select().from(sensor).where(eq(sensor.teacherId, teacherId));
+    sensors.forEach(async (sensorInfo, index) => {
+        const res = await db.update(sensor)
+            .set({value: param.value[index]})
+            .where(eq(sensor.id, sensors[index].id));
+    });
+//    param.value.forEach(async (value, index) => {
+//    });
+//    return Response.json(result);
 });
 
 app.get('/config', async (c) => {
