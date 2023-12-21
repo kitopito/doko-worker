eed26a28bc3d50551cabe5f05c9e425103b1ec31
kitopito <kitopitowada@gmail.com>
2023-12-21 13:48:31 +0900

    join by drizzle

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Hono } from 'hono'
 import { Bindings } from 'hono/types';
 import { drizzle } from 'drizzle-orm/d1';
-import { teacher, users } from './schema';
+import { status, teacher, users } from './schema';
+import { eq } from 'drizzle-orm';
 
 /*
 export interface Env {
@@ -15,9 +16,37 @@ type Env = {
 const app = new Hono<{Bindings: Env}>()
 
 app.get('/', async (c) => {
+    return new Response("hoge");
     const db = drizzle(c.env.DB);
     const result = await db.select().from(teacher).all();
     return Response.json(result);
+});
+
+app.get('/teachers', async (c) => {
+    /*
+    const db = drizzle(c.env.DB);
+    const result = await db.select().from(teacher).all();
+    return Response.json(result);
+    */
+
+    const db = drizzle(c.env.DB);
+    const result = await db.select({
+        id: teacher.id,
+        name: teacher.name,
+        status: status.status,
+    })
+    .from(teacher)
+    .leftJoin(status, eq(teacher.statusId, status.id));
+    return Response.json(result);
+});
+
+app.get('/status', async (c) => {
+});
+
+app.get('/sensors', async (c) => {
+});
+
+app.get('/config', async (c) => {
 });
 
 export default app
