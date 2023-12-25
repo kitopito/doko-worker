a6a5e7efbc1c67b9884f6111d9aa41efbc980545
kitopito <b19125@kagoshima.kosen-ac.jp>
2023-12-25 11:33:23 +0900

    update sensor value

--- src/index.ts
+++ src/index.ts
@@ -23,12 +23,6 @@ app.get('/', async (c) => {
 });
 
 app.get('/teachers', async (c) => {
-    /*
-    const db = drizzle(c.env.DB);
-    const result = await db.select().from(teacher).all();
-    return Response.json(result);
-    */
-
     const db = drizzle(c.env.DB);
     const result = await db.select({
         id: teacher.id,
@@ -41,12 +35,11 @@ app.get('/teachers', async (c) => {
     return Response.json(result);
 });
 
-app.get('/status', async (c) => {
-});
-
 app.post('/sensors', async (c) => {
     const db = drizzle(c.env.DB);
-    const param = await c.req.json<{deviceAddress: Number, value: Array<number>}>();
+//    console.log(c.req);
+    const param = 
+        await c.req.json<{deviceAddress: Number, value: Array<number>}>();
     console.log(param);
 //    const sensorValues = c.req.param('value');
 //    const teacherId = 2; // Mr.Taniguchi
@@ -58,12 +51,30 @@ app.post('/sensors', async (c) => {
             .set({value: param.value[index]})
             .where(eq(sensor.id, sensors[index].id));
     });
+    return new Response("OK");
 //    param.value.forEach(async (value, index) => {
 //    });
 //    return Response.json(result);
 });
 
+app.get('/sensors', async (c) => {
+    console.log("ほげ");
+    const db = drizzle(c.env.DB);
+    const res = db.insert(sensor).values([
+        {id: 1, teacherId: 1, value: 2000},
+        {id: 2, teacherId: 1, value: 2000},
+        {id: 3, teacherId: 1, value: 2000},
+        {id: 4, teacherId: 1, value: 2000},
+    ])
+    const result = await db.select()
+    .from(sensor).all();
+    return Response.json(result);
+});
+
 app.get('/config', async (c) => {
+});
+
+app.get('/status', async (c) => {
 });
 
 export default app
